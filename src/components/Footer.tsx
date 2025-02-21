263c0c2923372559b99ba3ab31f6d92f41fee758
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-21 22:49:50 +0330

    chore: Footer component margin-top added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ import { ArrowUpRight } from '@geist-ui/icons';
 
 const Footer = () => {
   return (
-    <footer className='flex items-center justify-between gap-x-4 max-w-prose mx-auto px-4 sm:px-0'>
+    <footer className='flex items-center justify-between gap-x-4 max-w-prose mx-auto px-4 sm:px-0 mt-6'>
       <nav className='flex items-center gap-x-6 text-sm dark:text-gray-400'>
         <Link
           rel='noopener nofollow noreferrer'
