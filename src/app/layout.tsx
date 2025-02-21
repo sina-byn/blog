20a016c7176586a4a33c79f085c9cc9c820c95f2
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-21 15:59:17 +0330

    build: path segment config added to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
+export const dyanmic = 'force-static';
+
 // * components
 import Footer from '@/components/Footer';
 import InlineScript from '@/components/InlineScript';
