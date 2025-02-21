e6e249abdb979b72ad8034f89e851b0dbf43761b
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-21 22:52:15 +0330

    fix: Header colors fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <header className='max-w-prose mx-auto px-4 sm:px-0 my-10'>
-      <nav className='flex flex-wrap items-center gap-x-6 gap-y-2 text-gray-200'>
+      <nav className='flex flex-wrap items-center gap-x-6 gap-y-2 dark:text-neutral-200'>
         <Link href='/'>home</Link>
         <Link href='/blog'>blog</Link>
       </nav>
