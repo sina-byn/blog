35a6fd302c77149a4f21d238cf1d97faf296ff09
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-21 16:39:58 +0330

    chore: navbar links colors updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,11 +9,11 @@ import { ArrowUpRight } from '@geist-ui/icons';
 const Footer = () => {
   return (
     <footer className='flex items-center justify-between gap-x-4 max-w-prose mx-auto px-4 sm:px-0'>
-      <nav className='flex items-center gap-x-6 text-sm'>
+      <nav className='flex items-center gap-x-6 text-sm dark:text-gray-400'>
         <Link
           rel='noopener nofollow noreferrer'
           href='https://github.com/sina-byn'
-          className='flex items-center gap-x-1'
+          className='flex items-center gap-x-1 dark:hover:text-white'
         >
           Github
           <ArrowUpRight size={18} />
@@ -22,7 +22,7 @@ const Footer = () => {
         <Link
           rel='noopener nofollow noreferrer'
           href='https://www.linkedin.com/in/sina-bayandorian/'
-          className='flex items-center gap-x-1'
+          className='flex items-center gap-x-1 dark:hover:text-white'
         >
           Linkedin
           <ArrowUpRight size={18} />
@@ -31,7 +31,7 @@ const Footer = () => {
         <Link
           rel='noopener nofollow noreferrer'
           href='https://github.com/sina-byn/blog'
-          className='flex items-center gap-x-1'
+          className='flex items-center gap-x-1 dark:hover:text-white'
         >
           Source
           <ArrowUpRight size={18} />
