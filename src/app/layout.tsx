fde4963f004ae135d95c8537a0d3aeb1b6e3f8d6
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-21 16:10:02 +0330

    fix: path segment config typo fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
-export const dyanmic = 'force-static';
+export const dynamic = 'force-static';
 
 // * components
 import Footer from '@/components/Footer';
