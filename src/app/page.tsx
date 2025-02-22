4ee1cfc7b7ed96b0f9cdcab81cee18fa830a4db3
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-22 14:19:04 +0330

    chore: landing page HTML added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
+// * utils
+import { getPosts } from '@/utils';
+
+// * components
+import PostLink from '@/components/PostLink';
+
 const LandingPage = () => {
-  return <div></div>;
+  const posts = getPosts(5);
+
+  return (
+    <main className='max-w-prose mx-auto px-4 sm:px-0'>
+      <hgroup className='prose dark:prose-invert mb-12'>
+        <h1>Sina Bayandorian</h1>
+        <p></p>
+
+        <hr />
+      </hgroup>
+
+      <section>
+        <div className='space-y-6'>
+          {posts.map(metadata => (
+            <PostLink key={metadata.slug} {...metadata} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default LandingPage;
