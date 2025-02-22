e85788a0d87612de87fc3d769d5c76619c42db6e
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-22 13:30:52 +0330

    chore: PostLink component added

---
+++ src/components/PostLink.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+// * types
+import type { Metadata } from '@/utils';
+
+type PostLinkProps = Pick<Metadata, 'slug' | 'title' | 'publishedAt'>;
+
+const PostLink = ({ slug, title, publishedAt }: PostLinkProps) => {
+  return (
+    <Link href={`/blog/${slug}`} className='grid lg:grid-cols-[auto_1fr] gap-x-4'>
+      <time dateTime={publishedAt} className='text-neutral-600 dark:text-neutral-400'>
+        {new Date(publishedAt).toLocaleDateString('en', {
+          month: 'short',
+          day: 'numeric',
+          year: 'numeric',
+        })}
+      </time>
+
+      <p>{title} Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, error?</p>
+    </Link>
+  );
+};
+
+export default PostLink;
