e01552dabe0b73deded151b2dff60252cee45c88
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-22 14:12:30 +0330

    chore: LICENSE and copyright added to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,8 @@
 import Link from 'next/link';
 
+// * pkg
+import pkg from '@/../package.json';
+
 // * components
 import ThemeSwitch from './ThemeSwitch';
 
@@ -8,37 +11,45 @@ import { ArrowUpRight } from '@geist-ui/icons';
 
 const Footer = () => {
   return (
-    <footer className='flex items-center justify-between gap-x-4 max-w-prose mx-auto px-4 sm:px-0 mt-6'>
-      <nav className='flex items-center gap-x-6 text-sm dark:text-gray-400'>
-        <Link
-          rel='noopener nofollow noreferrer'
-          href='https://github.com/sina-byn'
-          className='flex items-center gap-x-1 dark:hover:text-white'
-        >
-          Github
-          <ArrowUpRight size={18} />
-        </Link>
+    <footer className='max-w-prose mx-auto px-4 sm:px-0 mt-10'>
+      <div className='flex items-center justify-between gap-x-4'>
+        <nav className='flex items-center gap-x-6 text-sm dark:text-gray-400'>
+          <Link
+            rel='noopener nofollow noreferrer'
+            href='https://github.com/sina-byn'
+            className='flex items-center gap-x-1 dark:hover:text-white'
+          >
+            Github
+            <ArrowUpRight size={18} />
+          </Link>
 
-        <Link
-          rel='noopener nofollow noreferrer'
-          href='https://www.linkedin.com/in/sina-bayandorian/'
-          className='flex items-center gap-x-1 dark:hover:text-white'
-        >
-          Linkedin
-          <ArrowUpRight size={18} />
-        </Link>
+          <Link
+            rel='noopener nofollow noreferrer'
+            href='https://www.linkedin.com/in/sina-bayandorian/'
+            className='flex items-center gap-x-1 dark:hover:text-white'
+          >
+            Linkedin
+            <ArrowUpRight size={18} />
+          </Link>
 
-        <Link
-          rel='noopener nofollow noreferrer'
-          href='https://github.com/sina-byn/blog'
-          className='flex items-center gap-x-1 dark:hover:text-white'
-        >
-          Source
-          <ArrowUpRight size={18} />
-        </Link>
-      </nav>
+          <Link
+            rel='noopener nofollow noreferrer'
+            href='https://github.com/sina-byn/blog'
+            className='flex items-center gap-x-1 dark:hover:text-white'
+          >
+            Source
+            <ArrowUpRight size={18} />
+          </Link>
+        </nav>
 
-      <ThemeSwitch />
+        <ThemeSwitch />
+      </div>
+
+      {pkg?.license === 'MIT' && (
+        <div className='text-sm text-neutral-600 lg:text-neutral-400 mt-4'>
+          &copy; {new Date().getFullYear()} {pkg.license} Licensed
+        </div>
+      )}
     </footer>
   );
 };
