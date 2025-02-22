ff2e659cab51a6017a20282f615d7d1ebe01a534
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-22 14:18:25 +0330

    chore: read-time added to post page

--- src/app/blog/[post]/page.tsx
+++ src/app/blog/[post]/page.tsx
@@ -1,6 +1,11 @@
 // * utils
+import { getPost, calculateReadTime } from '@/utils';
+
+// * components
 import CustomMDX from '@/components/CustomMDX';
-import { getPost } from '@/utils';
+
+// * icons
+import { Clock } from '@geist-ui/icons';
 
 // * types
 type PostProps = { params: Promise<{ post: string }> };
@@ -12,6 +17,7 @@ export async function generateStaticParams() {
 const Post = async ({ params }: PostProps) => {
   const { post } = await params;
   const [metadata, mdx] = getPost(post);
+  const readTime = calculateReadTime(mdx);
 
   return (
     <main className='prose dark:prose-invert mx-auto px-4 sm:px-0'>
@@ -19,6 +25,15 @@ const Post = async ({ params }: PostProps) => {
         <h1>{metadata.title}</h1>
         <p>{metadata.description}</p>
       </hgroup>
+
+      {readTime !== 0 && (
+        <div className='flex items-center gap-x-2'>
+          <Clock size={16} className='stroke-neutral-600 dark:stroke-neutral-400' />
+          <span className='text-sm text-neutral-600 dark:text-neutral-400'>
+            {readTime} min. read
+          </span>
+        </div>
+      )}
 
       <hr />
 
