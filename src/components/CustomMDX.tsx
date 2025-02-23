81f61a584b195ec16b34c159c257378c3b78cd0c
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-23 07:36:52 +0330

    feat: syntax-highlighting added to code blocks

--- src/components/CustomMDX.tsx
+++ src/components/CustomMDX.tsx
@@ -1,12 +1,26 @@
 import { MDXRemote, type MDXRemoteProps } from 'next-mdx-remote/rsc';
 
+// * plugins
+import { highlight } from 'sugar-high';
+
 // * types
 type CustomMDXProps = Source;
 
 type Source = Pick<MDXRemoteProps, 'source'>;
 
+type CodeProps = { children: string };
+
 const CustomMDX = ({ source }: CustomMDXProps) => {
-  return <MDXRemote source={source} components={{}} />;
+  return (
+    <MDXRemote
+      source={source}
+      components={{
+        code: ({ children }: CodeProps) => {
+          return <code dangerouslySetInnerHTML={{ __html: highlight(children) }} />;
+        },
+      }}
+    />
+  );
 };
 
 export default CustomMDX;
