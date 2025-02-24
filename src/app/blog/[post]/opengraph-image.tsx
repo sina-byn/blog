fb8ac476d45ef1a25d96ceea2ed779c936d65a15
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-24 17:17:27 +0330

    feat: og image added

---
+++ src/app/blog/[post]/opengraph-image.tsx
@@ -0,0 +1,43 @@
+import { ImageResponse } from 'next/og';
+
+// * utils
+import { getPost } from '@/utils';
+
+// * types
+type ImageProps = { params: Promise<{ post: string }> };
+
+export const alt = 'About Acme';
+export const contentType = 'image/png';
+
+const Image = async ({ params }: ImageProps) => {
+  const { post } = await params;
+  const [metadata] = getPost(post);
+  const { title, publishedAt } = metadata;
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          background: 'black',
+          color: 'white',
+          fontSize: 48,
+          padding: '5rem',
+        }}
+      >
+        <p style={{ flexGrow: 1 }}>{title}</p>
+
+        <p style={{ fontSize: 28 }}>Sina Bayandorian</p>
+        <p style={{ fontSize: 24, marginTop: -40 }}>
+          {new Date(publishedAt).toLocaleDateString('en-US', { day: 'numeric', month: 'long' })}
+        </p>
+      </div>
+    ),
+    { width: 1200, height: 630 }
+  );
+};
+
+export default Image;
