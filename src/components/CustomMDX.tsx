183bc8b848f8570af7563d31f377214b561c7bb0
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-24 17:43:47 +0330

    feat: remarkToc included and configured

--- src/components/CustomMDX.tsx
+++ src/components/CustomMDX.tsx
@@ -1,6 +1,8 @@
 import { MDXRemote, type MDXRemoteProps } from 'next-mdx-remote/rsc';
 
 // * plugins
+import remarkToc from 'remark-toc';
+
 import { highlight } from 'sugar-high';
 
 // * types
@@ -14,6 +16,9 @@ const CustomMDX = ({ source }: CustomMDXProps) => {
   return (
     <MDXRemote
       source={source}
+      options={{
+        mdxOptions: { remarkPlugins: [[remarkToc, { heading: 'Table of Contents', maxDepth: 3 }]] },
+      }}
       components={{
         code: ({ children }: CodeProps) => {
           return <code dangerouslySetInnerHTML={{ __html: highlight(children) }} />;
