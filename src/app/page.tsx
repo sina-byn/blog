d949118093f5da07403b5b570c83f0c09464a705
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-25 22:42:33 +0330

    chore: landing page bio added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,13 @@ const LandingPage = () => {
     <main className='max-w-prose mx-auto px-4 sm:px-0'>
       <hgroup className='prose dark:prose-invert mb-12'>
         <h1>Sina Bayandorian</h1>
-        <p></p>
+
+        <p>
+          I'm a front-end developer specializing in React and Vue, with a strong interest in
+          exploring the depths of Node.js. I believe in expanding my tech stack—why not code in Go?
+          I also have a deep appreciation for C++ and am eager to dive into Rust. Well, isn't that a
+          lot to learn? Then let's get started right away, as time is short.
+        </p>
 
         <hr />
       </hgroup>
