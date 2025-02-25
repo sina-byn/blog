29b6ffd8a61f652df7bd1b3ae97385259aed47b2
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-25 22:49:38 +0330

    chore: eslint disable/enable comments added to avoid unescaped chars error during build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,14 @@ const LandingPage = () => {
       <hgroup className='prose dark:prose-invert mb-12'>
         <h1>Sina Bayandorian</h1>
 
+        {/* eslint-disable */}
         <p>
           I'm a front-end developer specializing in React and Vue, with a strong interest in
           exploring the depths of Node.js. I believe in expanding my tech stack—why not code in Go?
           I also have a deep appreciation for C++ and am eager to dive into Rust. Well, isn't that a
           lot to learn? Then let's get started right away, as time is short.
         </p>
+        {/* eslint-enable */}
 
         <hr />
       </hgroup>
