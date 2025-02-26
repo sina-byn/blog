0c20a53d175cb8777d170297acc8f191d397861d
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-26 12:19:36 +0330

    fix: InlineScript component exception handling fixed to avoid 500 errors

--- src/components/InlineScript.tsx
+++ src/components/InlineScript.tsx
@@ -4,7 +4,10 @@ import fs from 'fs';
 type InlineScriptProps = { src: string };
 
 const InlineScript = ({ src }: InlineScriptProps) => {
-  if (!fs.existsSync(src)) throw new Error(`Could not find '${src}'`);
+  if (!fs.existsSync(src)) {
+    console.error(`Could not find '${src}'`);
+    return null;
+  }
 
   const script = fs.readFileSync(src, 'utf-8');
   return <script>{script}</script>;
