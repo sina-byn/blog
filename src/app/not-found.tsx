1cb059c7ca7022a7bfa590cb4d11e28cf0cd8afa
sina-byn <sina.sina.bayan@protonmail.com>
2025-02-26 12:01:38 +0330

    feat: not-found page added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+const NotFound = () => {
+  return (
+    <div className='flex flex-col items-center justify-center fixed inset-0 z-10 bg-background text-center'>
+      <div>
+        <h1 className='inline-block text-2xl font-medium align-top leading-12 border-r border-blac/30 dark:border-white/30 mr-5 pr-6'>
+          404
+        </h1>
+        <div className='inline-block'>
+          <h2 className='text-sm font-normal leading-12 m-0'>This page could not be found.</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
